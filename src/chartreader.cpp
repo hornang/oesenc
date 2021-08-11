b10e5cd5b9d9dfa736bf5d0bef235e94b789954f
Stig Hornang <shornang@gmail.com>
2021-08-11 21:53:36 +0200

    Fix reading on Windows
    Must read in binary mode and must check for eof (at least for now)

--- src/chartreader.cpp
+++ src/chartreader.cpp
@@ -2,7 +2,7 @@
 
 bool ChartReader::Open(const std::string &fileName)
 {
-    m_file = std::ifstream(fileName);
+    m_file = std::ifstream(fileName, std::ios::binary);
 
     if (!m_file.is_open()) {
         std::cout << "Unable to open file" << std::endl;
@@ -19,5 +19,9 @@ bool ChartReader::Read(void *buffer, size_t size)
     }
     m_file.read(reinterpret_cast<char *>(buffer), size);
     bool result = (m_file.rdstate() & std::ifstream::goodbit) == 0;
+    const bool eof = (m_file.rdstate() & std::ifstream::eofbit);
+    if (eof) {
+        return false;
+    }
     return result;
 }
